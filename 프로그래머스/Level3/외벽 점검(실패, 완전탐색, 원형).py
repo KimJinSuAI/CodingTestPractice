1fffe8b321ee6d81e7245bb427838a9b21d3a490
jinsu <wlstnwkwjsrj@naver.com>
2021-07-29 17:38:12 +0900

    21.07.29

--- 프로그래머스/Level3/외벽 점검(실패, 완전탐색, 원형).py
+++ 프로그래머스/Level3/외벽 점검(실패, 완전탐색, 원형).py
@@ -1,3 +1,10 @@
+#완전탐색인지 판단하려면 제한사항의 변수범위가 크지않아야함
+#완전탐색은 가능한 변수들을 모두 찾아야함
+#1. 시작지점어디? -> 모든 지점시작 후 삭제안된 지점부터..
+#2. 어떤친구로 결함찾을까? -> 모든 친구순열에서 구함
+#3. 시계방향? 반시계방향? -> ****원형을 직선으로 만듦. 기존 배열*2****
+#ex) n==12일 때, 1(13)->10 == 10->1(13)
+
 from itertools import permutations
 def solution(n, weak, dist):
     dist = sorted(dist) #오름차순으로 주어지는건가?
@@ -23,6 +30,6 @@ def solution(n, weak, dist):
         return -1
     else:
         return min(answer)
-# print(solution(12, [1, 5, 6, 10], [1, 2, 3, 4]))
-print(solution(12, [1, 3, 4, 9, 10], [3, 5, 7]))
+print(solution(12, [1, 5, 6, 10], [1, 4]))
+# print(solution(12, [1, 3, 4, 9, 10], [3, 5, 7]))
 # print(solution(12,[10,0],[1,2]))

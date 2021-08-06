b05b06dac0015827a3add75db36c3e741a6d3a7b
jinsu <wlstnwkwjsrj@naver.com>
2021-08-07 05:18:25 +0900

    21.08.06

--- 프로그래머스/Level4/미로 탈출(실패, dijkstra, 그래프의상태).py
+++ 프로그래머스/Level4/미로 탈출(실패, dijkstra, 그래프의상태).py
@@ -1,44 +1,29 @@
-from collections import deque
+from itertools import permutations
+import heapq
+import copy
 def solution(n, start, end, roads, traps):
-    answer = -1
-    miro = {}
-    for i,road in enumerate(roads):
-        miro[road[0]] = miro.get(road[0],{})
-        miro[road[0]][road[1]] = miro[road[0]].get(road[1],[])+[[i,road[2]]]
-        miro[road[1]] = miro.get(road[1],{})
-        miro[road[1]][road[0]] = miro[road[1]].get(road[0],[])+[[i,-1]]
-    Q = deque()
-    Q.append((start,0,miro.copy()))
+    maze = {}
+    visited = [[False]*n for _ in range(1<<len(traps))]
+    for road in roads:
+        maze[road[0]] = maze.get(road[0],[])+[[road[1],road[2],0]]
+        maze[road[1]] = maze.get(road[1],[])+[[road[0],-1,1]]
+    Q = []
+    heapq.heappush(Q, (0,start,0))
+    
     while Q:
-        node = Q.popleft()
-        if answer!=-1 and node[1]>=answer:
+        cost, node, state = heapq.heappop(Q) #비용, 노드, 상태
+        if node==end:
+            return cost
+        if visited[state][node]:
             continue
-        elif node[0]==end:
-            answer = node[1]
-            continue
-        if node[0] in traps:   #trap
-            for road in node[2][node[0]].items():#현재다리에 연결된 노드별 처리. road[0] = 노드, road[1] = [번호, 가중치]
-                for r in road[1]:                #노드에 연결된 다리별 처리
-                    for tmp in node[2][road[0]].items():    #상대 노드와 같은번호의 다리 찾아 처리
-                        if tmp[0]==node[0]:     #상대의 현재노드찾음
-                            for x in tmp[1]:    #상대노드에서도 같은 번호의 다리 찾음
-                                if x[0]==r[0]:
-                                    if r[1]==-1:
-                                        r[1] = x[1]
-                                        x[1] = -1
-                                    else:
-                                        x[1] = r[1]
-                                        r[1] = -1
-                                    break
-                            break
-           
+        visited[state][node] = True
 
-        for road in node[2][node[0]].items():
-            for r in road[1]:
-                if r[1]==-1:
-                    continue
-                Q.append((road[0],node[1]+r[1],node[2].copy()))
-    return answer
-# print(solution(4,1,4,[[1, 2, 1], [3, 2, 1], [2, 4, 1]],[2, 3]))
+        for nextNode in maze[node]:
+            if nextNode[1]==0:
+                if nextNode[0] in traps:
 
-print(solution(4,1,3,[[1, 2, 1], [4, 3, 1], [4, 2, 1]],[2,4]))
+                heapq.heappush(Q, (nextNode[0],nextNode[1],state))
+            
+    return -1
+
+print(solution(4,1,4,[[1, 2, 1], [3, 2, 1], [2, 4, 1]],[2, 3]))

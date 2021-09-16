94e31592c4c493ff96696429e9617e272197ef1c
김진수 <wlstnwkwjsrj@naver.com>
2021-09-16 17:02:45 +0900

    21.09.16

--- 프로그래머스/Level4/안티 세포.py
+++ 프로그래머스/Level4/안티 세포.py
@@ -1,17 +1,35 @@
 import math
 def solution(a, s):
+    def get_b(b):
+        N = len(b)
+        i = 1
+        ct = 1
+        PS = [0]
+        for tmp in b:
+            PS.append(PS[-1]+tmp)
+        DP = [[0]*(math.floor(math.log2(max(b)))+1) for _ in range(N)]
+        DP[0][0] = 1        #DP[0][0] = 1
+        while i<N:
+            DP[i][b[i]] = (ct,i-1)
+            var = b[i]
+            j = i-1
+            while DP[j].get(var,None)!=None:
+                diff = DP[j][var]
+                ct += diff[0]
+                var *= 2
+                DP[i][var] = diff
+                j = diff[1]
+                if j == None:
+                    break
+            i+=1
+        return ct%(10**9 + 7)
+
+
     answer = []
-    i = 0
-    for j in s:
-        tmp = a[i:i+j]
-        maxE = math.floor(math.log2(max(tmp)))+1
-        DP = [[0]*maxE for _ in range(len(tmp))]    #DP정의
-
-        DP[0][0] = 1 #기저조건
-        
-
-        answer.append((tmp, DP))
-        i+=j
-        
+    subsum = 0
+    for x in s:
+        b = a[subsum:subsum+x]
+        subsum+=x
+        answer.append(get_b(b))
     return answer
 print(solution(	[1, 1, 1, 1, 1, 1, 2, 5, 8, 2, 1, 1, 4, 8, 8, 8, 12, 6, 6], [4, 3, 1, 5, 6]))
